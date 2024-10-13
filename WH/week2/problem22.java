e30e15b79486a74f1175913dd870bcfb1c7d920e
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2024-10-13 19:01:17 +0900

    problem22.java

--- WH/week2/problem22.java
+++ WH/week2/problem22.java
@@ -5,10 +5,13 @@ class Solution {
         int sum = 0;
         Arrays.sort(score);
         int n = score.length;
-        
+        int numOfBox = n / m; // 만들어질 상자의 수
         for(int i = n - m; i >= 0; i-=m){
-            sum += score[i] * m;
+            int maxProfit = 0;
+            maxProfit = score[i] * m;
+            sum += maxProfit;
         }
         return sum;
     }
+}
 }
