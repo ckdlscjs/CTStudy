fa2023ce01fe68eadba44321e7a6f5e08bd218ce
Jin Ho Jeon <34098750+JJH123123123@users.noreply.github.com>
2024-12-15 22:37:41 +0900

    change name and gitignore

--- JH/week9/Problem175.java
+++ JH/week9/Problem175.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class problem175 {
+public class Problem175 {
     public static void main(String[] args) 
     throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
